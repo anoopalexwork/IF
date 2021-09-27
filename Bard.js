c3d9f9c5d5026a64f9cc644a73c4817c000f8bad
Vasu Devan <anoopalexwork@gmail.com>
2021-09-27 08:46:33 +0530

    split up js files to prevent huge if.html file

---
+++ Bard.js
@@ -0,0 +1,37 @@
+tag = "";
+guild = -1;
+village = -1;
+myroom= -1;
+locwas = "start";
+story = document.getElementById("story");
+choose = document.getElementById("choose");
+
+
+
+//Add HTML text to div story
+function add(x) { story.innerHTML += x+"<br>"; }
+//Display button with text x and onclick string f, in div choose
+function pick(x,f) { choose.innerHTML += "<button onclick=' chose(this);"+f+"'>"+x+"</button><br>"; }
+//Convert element x text to div with green text, and clear choose div
+function chose(x) { 
+					add("<br><div style='color:green'>"+ x.innerText +"</div><br><br>");
+					choose.innerHTML ="";
+}
+//Add tag x
+function set(x) { tag+=x+" "; }
+//If found x in tag var, return true else false
+function have(x) { if (tag.search(x) >= 0) return true; else return false; }
+//Del tag x from tag, display err if x not found
+function del(x) { 
+	chg = tag.replace(x+" ","");
+	if (chg == tag) {
+		add("<br>ERR:" + x +  " not found!");
+		
+	}
+	tag = chg;
+}
+
+
+
+//Begin story
+Guild();
---
+++ Village.js
@@ -0,0 +1,21 @@
+function Village(x) {
+		if (x==null) village++; else village=x;
+		
+		if (village==0) {
+			set("goVillage");
+			add("I leave the gate and walk away down a nearby dirt road. Soon, I hear the sounds of a small, nearby village. It is bustling with activity. Plainly dressed people walk to and fro. Women carry geese and fruits as they move amongst the shacks. Men carry rocks or large sack of grain and dung. The inn looks inviting with the smell of ale and wine.....but there are also mercs carrying swords and axes.<br>");
+				pick("Upon entering the inn....","Village()");
+				pick("I return to the Guild", "Guild()");
+		}
+		if (village==1) {
+			add("I get drunk.<br>");
+			pick("Return to Guild","Guild()");
+			pick("Make out with barmaid","Village()");
+		}
+		if (village==2) {
+			add("Me and Milly the barmaid flirt for a bit.");
+			set("flirt");
+			pick("Then, I return to the Guild.","Guild()");
+		}
+	
+	}
